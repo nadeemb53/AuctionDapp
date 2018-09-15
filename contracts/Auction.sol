f6912ef154374873c2eda27a5393020848635686
Nadeem Bhati <nadeemb53@gmail.com>
2018-09-15 16:46:39 +0530

    added text.js and auction.sol smart contract

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -28,22 +28,15 @@ contract Auction {
     //functions
 
     function Auction() public payable{    //constructor
-                
-        //Part 1 Task 1. Initialize beneficiary with address of smart contract’s owner
-        //Hint. In the constructor,"msg.sender" is the address of the owner.
-        // ** Start code here. 1 line approximately. **/
 	beneficiary = msg.sender;
-          //** End code here. **/
+         
         uint[] memory emptyArray;
         items[0] = Item({itemId:0,itemTokens:emptyArray});
         
-        //Part 1 Task 2. Initialize two items with at index 1 and 2. 
-        // ** Start code here. 2 lines approximately. **/
-	uint[] memory emptyArray1;
+	    uint[] memory emptyArray1;
         items[1] = Item({itemId:1,itemTokens:emptyArray1});
         uint[] memory emptyArray2;
         items[2] = Item({itemId:2,itemTokens:emptyArray2});
-        //** End code here**/
     }
     
 
@@ -51,13 +44,8 @@ contract Auction {
         
         
         bidders[bidderCount].personId = bidderCount;
-        
-        //Part 1 Task 3. Initialize the address of the bidder 
-        /*Hint. Here the bidders[bidderCount].addr should be initialized with address of the registrant.*/
 
-        // ** Start code here. 1 line approximately. **/
-	bidders[bidderCount].addr = msg.sender;
-        //** End code here. **
+	    bidders[bidderCount].addr = msg.sender;
         
         bidders[bidderCount].remainingTokens = 5; // only 5 tokens
         tokenDetails[msg.sender]=bidders[bidderCount];
@@ -71,27 +59,10 @@ contract Auction {
             _itemId -- uint, id of the item
             _count -- uint, count of tokens to bid for the item
         */
-        
-        /*
-        Part 1 Task 4. Implement the three conditions below.
-            4.1 If the number of tokens remaining with the bidder is < count of tokens bidded, revert.
-            4.2 If there are no tokens remaining with the bidder, revert.
-            4.3 If the id of the item for which bid is placed, is greater than 2, revert.
-
-        Hint: "tokenDetails[msg.sender].remainingTokens" gives the details of the number of tokens remaining with the bidder.
-        */
-        
-        // ** Start code here. 2 lines approximately. **/
     	if(tokenDetails[msg.sender].remainingTokens <_count || tokenDetails[msg.sender].remainingTokens <=0){revert();}
 	if(_itemId>items.length-1){revert();}
-        //** End code here. **
-        
-        /*Part 1 Task 5. Decrement the remainingTokens by the number of tokens bid and store the value in balance variable.
-        Hint. "tokenDetails[msg.sender].remainingTokens" should be decremented by "_count". */
- 
-        // ** Start code here. 1 line approximately. **
+
         uint balance= tokenDetails[msg.sender].remainingTokens - _count;
-        //** End code here. **
         
         tokenDetails[msg.sender].remainingTokens=balance;
         bidders[tokenDetails[msg.sender].personId].remainingTokens=balance;//updating the same balance in bidders map.
@@ -102,13 +73,9 @@ contract Auction {
         }
     }
     
-    // Part 2 Task 1. Create a modifier named "onlyOwner" to ensure that only owner is allowed to reveal winners
-    //Hint : Use require to validate if "msg.sender" is equal to the "beneficiary".
     modifier onlyOwner {
-        // ** Start code here. 2 lines approximately. **
         require(msg.sender == beneficiary);
         _;
-        //** End code here. **
     }
     
     
@@ -127,20 +94,13 @@ contract Auction {
 
             uint winnerId = currentItem.itemTokens[randomIndex];
                 
-            /* Part 1 Task 6. Assign the winners.
-            Hint." bidders[winnerId] " will give you the person object with the winnerId.
-            you need to assign the address of the person obtained above to winners[id] */
-
-            // ** Start coding here *** 1 line approximately.
             winners[id] = bidders[winnerId].addr;
                     
-            //** end code here*
                 
             }
         }
     } 
 
-  //Miscellaneous methods: Below methods are used to assist Grading. Please DONOT CHANGE THEM.
     function getPersonDetails(uint id) public constant returns(uint,uint,address){
         return (bidders[id].remainingTokens,bidders[id].personId,bidders[id].addr);
     }
--- test/test.js
+++ test/test.js
@@ -25,11 +25,6 @@ contract('AuctionContract', function (accounts) {
 
   //Test Case for checking if the bid is more than the token amount
   it("Should NOT allow to bid more than remaining tokens", function() {
-    /**********
-    TASK 1:   Call bid method from accounts[1] of Auction.sol using auctionInstance and
-    pass itemId=0, count=6 as arguments
-    HINT:     To make a function call from account 1 use {from: accounts[1]} as an extra argument
-    ***********/
     return auctionInstance.bid({from:accounts[1]},itemId=0, count=6)
     .then(function (result) {
       /*
@@ -39,16 +34,8 @@ contract('AuctionContract', function (accounts) {
     }).catch(function (e) {
       var a = e.toString();
       if(e === "Failed to check remaining tokens less than count") {
-        /**********
-        TASK 2: This is the error which we had thrown. Should you assert true or false?
-        HINT:   Use assert(false) to assert false
-                Use assert(true) to assert true
-        ***********/
         assert(false);
       } else {
-        /**********
-        TASK 3: assert the opposite here
-        ***********/
         assert(true);
       }
     })
@@ -56,9 +43,6 @@ contract('AuctionContract', function (accounts) {
 
   //Modifier Checking
   it("Should NOT allow non owner to reveal winners", function() {
-    /**********
-    TASK 4: Call revealWinners from account 1
-    ***********/
      return auctionInstance.revealWinners({from:accounts[1]})
      .then(function (instance) {
        /*
@@ -67,16 +51,8 @@ contract('AuctionContract', function (accounts) {
        throw("Failed to check owner in reveal winners");
      }).catch(function (e) {
        if(e === "Failed to check owner in reveal winners") {
-         /**********
-         TASK 5: This is the error which we had thrown. Should you assert true or false?
-         HINT:   Use assert(false) to assert false
-                 Use assert(true) to assert true
-         ***********/
          assert(false);
        } else {
-         /**********
-         TASK 6: assert the opposite here
-         ***********/
          assert(true);
        }
      })
@@ -84,79 +60,28 @@ contract('AuctionContract', function (accounts) {
 
 
   it("Should set winners", function() {
-    /**********
-    TASK 7: Call register function from account 2
-    ***********/
     return auctionInstance.register({from:accounts[2]})
     .then(function(result) {
-      /**********
-      TASK 8: Call register function from account 3
-      ***********/
         return auctionInstance.register({from:accounts[3]})
     }).then(function() {
-      /**********
-      TASK 9: Call register function from account 4
-      ***********/
         return auctionInstance.register({from:accounts[4]})
     }).then(function() {
-      /**********
-      TASK 10: Call bid method from accounts[2] of Auction.sol using auctionInstance and
-      pass itemId=0, count=5 as arguments
-      ***********/
         return auctionInstance.bid(itemId=0,count=5,{from:accounts[2]})
     }).then(function() {
-      /**********
-      TASK 11: Call bid method from accounts[3] of Auction.sol using auctionInstance and
-      pass itemId=1, count=5 as arguments
-      ***********/
         return auctionInstance.bid(itemId=1,count=5,{from:accounts[3]})
     }).then(function() {
-      /**********
-      TASK 12: Call bid method from accounts[4] of Auction.sol using auctionInstance and
-      pass itemId=2, count=5 as arguments
-      ***********/
         return auctionInstance.bid(itemId=2,count=5,{from:accounts[4]})
     }).then(function() {
-      /**********
-      TASK 13: Call revealWinners function from accounts[0]
-      ***********/
         return auctionInstance.revealWinners({from:accounts[0]})
     }).then(function() {
-      /**********
-      TASK 14: call winners function from accounts[0] to get the winner of item id 0
-      ***********/
         return auctionInstance.winners(itemId=0,{from:accounts[0]})
     }).then(function(result) {
-      /**********
-      TASK 15:  assert to see if the winner address is not the default address
-      HINT:     Default address is '0x0000000000000000000000000000000000000000'
-                Use notEqual method of assert
-                Parameters for notEqual : (result, default address , message);
-      ***********/
       assert.notEqual(result,'0x0000000000000000000000000000000000000000','failed');
-      /**********
-      TASK 16: call winners function from accounts[0] to get the winner of item id 1
-      ***********/
       return auctionInstance.winners(itemId=1,{from:accounts[0]})
     }).then(function(result) {
-      /**********
-      TASK 17:  assert to see if the winner address is not the default address
-      HINT:     Default address is '0x0000000000000000000000000000000000000000'
-                Use notEqual method of assert
-                Parameters for notEqual : (result, default address , message);
-      ***********/
       assert.notEqual(result,'0x0000000000000000000000000000000000000000','failed');
-      /**********
-      TASK 18: Call winners function from account 3 to get the winner of item id 2
-      ***********/
       return auctionInstance.winners(itemId=2,{from:accounts[3]})
     }).then(function(result) {
-      /**********
-      TASK 19:  assert to see if the winner address is not the default address
-      HINT:     Default address is '0x0000000000000000000000000000000000000000'
-                Use notEqual method of assert
-                Parameters for notEqual : (result, default address , message);
-      ***********/
       assert.notEqual(result,'0x0000000000000000000000000000000000000000','failed');
     })
   });
